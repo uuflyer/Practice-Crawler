e9e547cbffd708e7c5084159a7f2f0257ddbabd2
kungua <52815012+kungua@users.noreply.github.com>
2019-09-04 15:01:10 +0800

    synchronized 方法 (#1)
    synchronized 方法 (#1)

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -3,19 +3,25 @@ package com.github.hcsp.multithread;
 public class Counter {
     private int value = 0;
 
+    private static final Object lock = new Object();
+
     public int getValue() {
         return value;
     }
 
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
-        value += i;
+        synchronized (lock) {
+            value += i;
+        }
         return value;
     }
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
-        value -= i;
+        synchronized (lock) {
+            value -= i;
+        }
         return value;
     }
 }
