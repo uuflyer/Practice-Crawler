13e481387c297c09a2338c030f210533d092c24f
dsying <dingduoduo0916@gmail.com>
2019-09-09 22:58:27 +0800

    synchronized 同步块保证同一时间只能有一个线程进入同步块 (#4)
    synchronized 同步块保证同一时间只能有一个线程进入同步块 (#4)

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -3,18 +3,18 @@ package com.github.hcsp.multithread;
 public class Counter {
     private int value = 0;
 
-    public int getValue() {
+    public synchronized int getValue() {
         return value;
     }
 
     // 加上一个整数i，并返回加之后的结果
-    public int addAndGet(int i) {
+    public synchronized int addAndGet(int i) {
         value += i;
         return value;
     }
 
     // 减去一个整数i，并返回减之后的结果
-    public int minusAndGet(int i) {
+    public synchronized int minusAndGet(int i) {
         value -= i;
         return value;
     }
